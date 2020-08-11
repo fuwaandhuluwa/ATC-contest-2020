d830fe78704960398b41baa4288ce18ceebd2626
fuwaandhuluwa <jiafuli2018@163.com>
2020-08-11 23:08:22 +0800

    [#4] using line_profiler module to analysis the performance results which we can be find which statement takes up more resources

--- py/performance_analysis.py
+++ py/performance_analysis.py
@@ -23,8 +23,18 @@
 # snakeviz status.txt
 
 
+# **************************************************************
+#  Line_profiler:
+#        using Line_profiler to analysis the results and find
+#        out which statement takes up more resources
+# **************************************************************
 # from line_profiler import LineProfiler
 # lp = LineProfiler()
 # lp_wrapper = lp(run)
 # lp_wrapper(readFileContent("C:/Users/jiafu.li/PyCharmProjects/AStar_Algorithm/testcase/1.in"))
 # lp.print_stats()
+# **************************************************************
+# 优化思路：
+#        1. adjacent 和判断是否为None占用15%的时间消耗，可以将其放在map初始化generateRoadMap()中一次性做完
+#        2. 尽量使用减少函数调用
+# **************************************************************
